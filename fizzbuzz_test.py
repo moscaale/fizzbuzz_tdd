32774c3c8f076642031dc7eb0a92fc3a96acad9e
a.mosca <a.mosca@octo.com>
2021-11-18 18:33:29 +0100

    FEAT: Add template

---
+++ fizzbuzz_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_(self):
+        # Given
+        expected = None
+    
+        # When
+        actual = None
+    
+        # Then
+        assert actual == expected
+    
+
+if __name__ == '__main__':
+    unittest.main()
