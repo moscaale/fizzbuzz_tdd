5c72388fa9628c336ffd1aec4228b7fce44b1ab8
Lea Nacccache <lea.naccache@octo.com>
2021-11-21 16:13:12 +0100

    add new final solution

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+
+
+def fizzbuzz(input_value):
+    if input_value % 3 == 0:
+        return "fizz"
+    elif input_value % 5 == 0:
+        return "buzz"
+    else:
+        return input_value
+
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,16 +1,85 @@
 import unittest
 
+from fizzbuzz import fizzbuzz
+
 
 class MyTestCase(unittest.TestCase):
-    def test_(self):
+    def test_fizzbuzz__given_1_return_1(self):
         # Given
-        expected = None
+        expected = 1
+        input_value = 1
     
         # When
-        actual = None
+        actual = fizzbuzz(input_value)
     
         # Then
         assert actual == expected
+
+    def test_fizzbuzz_given_2_return_2(self):
+        # Given
+        expected = 2
+        input_value =2
+        # When
+        actual = fizzbuzz(input_value)
+        # Then
+        assert actual == expected
+
+    def test_fizzbuzz__given_3_return_fizz(self):
+        # Given
+        expected = "fizz"
+        input_value = 3
+
+        # When
+        actual = fizzbuzz(input_value)
+
+        # Then
+        assert actual == expected
+
+    def test_fizzbuzz__given_4_return_4(self):
+        # Given
+        expected = 4
+        input_value = 4
+
+        # When
+        actual = fizzbuzz(input_value)
+
+        # Then
+        assert actual == expected
+
+    def test_fizzbuzz_given_5_return_buzz(self):
+        # Given
+        expected = 'buzz'
+        input_value = 5
+
+        # When
+        actual = fizzbuzz(input_value)
+
+        # Then
+        assert actual == expected
+
+    def test_fizzbuzz_given_6_return_fizz(self):
+        # Given
+        expected = "fizz"
+        input_value =6
+        # When
+        actual = fizzbuzz(input_value)
+        # Then
+        assert actual == expected
+
+    def test_fizzbuzz__given_10_return_buzz(self):
+        # Given
+        expected = "buzz"
+        input_value = 10
+        # When
+        actual = fizzbuzz(input_value)
+        # Then
+        assert actual == expected
+
+
+
+
+
+
     
 
 if __name__ == '__main__':
